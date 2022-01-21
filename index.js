acaeda14cb60e335a02d69f055214868d7be4db3
Erick Castille <ecastille924@gmail.com>
2022-01-20 19:30:22 -0500

    creating hover affect over cat image

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
     btnClick()
 })
 
+function fetchCatImage(){
+    let image = document.getElementById("cat-image")
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(resp => resp.json())
+    .then(json => image.src = json[0].url)
+}
+
 function btnClick(){
     let button = document.getElementById("new-cat-btn")
     button.addEventListener("click", fetchCatImage)
@@ -16,12 +23,6 @@ function fetchCatFacts(){
     .then(json => 
         renderCatFact(json.data)
     )
-}
-function fetchCatImage(){
-    let image = document.getElementById("cat-image")
-    fetch('https://api.thecatapi.com/v1/images/search')
-    .then(resp => resp.json())
-    .then(json => image.src = json[0].url)
 }
 
 function renderCatFact(facts){
